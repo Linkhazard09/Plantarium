9a70339c674233164c47601d588c8de11a5d3bd2
MATTHEW PALADINE RIVERA <mpprivera@mymail.mapua.edu.ph>
2020-08-30 02:58:45 +0800

    Add project files.

---
+++ Plantarium/Plantarium/Login.xaml.cs
@@ -0,0 +1,46 @@
+﻿using DotNetService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Plantarium
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        IService1 Ws = new DotNetService.Service1Client();
+        public Login()
+        {
+            InitializeComponent();
+            
+        
+            
+          
+        }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            string Username = EntryUsername.Text;
+            string Password = EntryPassword.Text;
+            
+            if (Ws.AccountAuthenticate(Username, Password) == true)
+            {
+               DisplayAlert("Correct", "Account Authenticated", "OK");
+            }
+           else
+                DisplayAlert("Incorrect", "Account does not exist", "OK");
+
+
+        }
+
+        private void SignUpButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
